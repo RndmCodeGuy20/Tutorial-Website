9423d1b784092b3ce988c5d049ab7705aa84fbcc
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2022-01-11 12:10:57 +0530

    first commit : master

---
+++ Code/HelloWord.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 19;
+    abs(23.08);
+
+    printf("My age is : %d\n", age);
+    printf("Hello World!\n");
+
+    return 0;
+}

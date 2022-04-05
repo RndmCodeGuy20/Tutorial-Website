f1923e3d51cff4f9bc678ebe010a379366627a3a
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2022-04-05 07:52:05 +0530

    npm server changes : master 🦈

---
+++ server.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server Started at port 3000!!!");
+});

a6c18453a3567fea4531192930277a1750a3596e
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2022-04-06 11:28:22 +0530

    perf: :zap: modules updated
    Node modules updated with bodyParser and request, file path not set : master

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 //jshint esversion:6
 
 const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/Fundamentals", function (req, res) {
+  res.sendFile(__dirname + "/Fundamentals/KYM.html");
 });
 
 app.listen(3000, function () {
---
+++ temp.js
@@ -0,0 +1,1 @@
+console.log(__dirname + "/Fundamentals/KYM.html");

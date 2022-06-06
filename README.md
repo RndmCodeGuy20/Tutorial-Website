9ba137473eb020be8774c56a613336322562e3c7
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-06-06 22:28:03 +0530

    docs: :hammer: src files updated, global paths changed, contribution info added : master

--- README.md
+++ README.md
@@ -4,10 +4,21 @@ HTML, CSS, JS based website to teach young minds programming and C.
 
 ## Installation
 
+Fork the repository
+
+Clone the repository on your local machine
+```
+https://github.com/RndmCodeGuy20/Tutorial-Website.git
+```
+Move to the cloned repository
+```
+cd Tutorial-Website/
+```
+
 Use the node package manager (npm) to run the server.
 
 ```bash
-node server.js
+npm start
 ```
 
 ## Images
@@ -17,6 +28,18 @@ node server.js
 ![Code Page](public/Assets/hello.jpg)
 
 ## Contributing
+
+### 1. For Content Creators :
+
+1. Fork and clone this repository on your local machine.
+2. Head to ./Drafts directory and create a markdown file and write your content into it.
+> Nomenclature for content file should be `Topic-<Your Name>`<br>
+> 💀 Do not append your data directly into the `HTML` file.
+
+### 2. For Developers :
+
+1. Fork and clone this repository on your local machine.
+2. Head to the file you want to contribute to, add features, insert content into markup, add or update tests or change src file to deploy the app.
 
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
--- server.js
+++ server.js
@@ -1,37 +1,45 @@
 //jshint esversion:6
 
-const express = require("express");
-const request = require("request");
-const bodyParser = require("body-parser");
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/index.html", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/index.html', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/Fundamentals/KYM.html", function (req, res) {
-  res.sendFile(__dirname + "/Fundamentals/KYM.html");
+app.get('/Fundamentals/KYM', function (req, res) {
+	res.sendFile(__dirname + '/Fundamentals/KYM.html');
 });
 
-app.get("/Fundamentals/ComputerLang.html", function (req, res) {
-  res.sendFile(__dirname + "/Fundamentals/ComputerLang.html");
+app.get('/Basics/About', function (req, res) {
+	res.sendFile(__dirname + '/Basics/About.html');
 });
 
-app.get("/Fundamentals/Compiler.html", function (req, res) {
-  res.sendFile(__dirname + "/Fundamentals/Compiler.html");
+app.get('/Construction', function (req, res) {
+	res.sendFile(__dirname + '/construction.html');
 });
 
-app.get("/Fundamentals/HelloWorld.html", function (req, res) {
-  res.sendFile(__dirname + "/Fundamentals/HelloWorld.html");
+app.get('/Fundamentals/ComputerLang', function (req, res) {
+	res.sendFile(__dirname + '/Fundamentals/ComputerLang.html');
+});
+
+app.get('/Fundamentals/Compiler', function (req, res) {
+	res.sendFile(__dirname + '/Fundamentals/Compiler.html');
+});
+
+app.get('/Fundamentals/HelloWorld', function (req, res) {
+	res.sendFile(__dirname + '/Fundamentals/HelloWorld.html');
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server Started at port 3000!!!");
+	console.log('Server Started at port http://localhost:3000');
 });
